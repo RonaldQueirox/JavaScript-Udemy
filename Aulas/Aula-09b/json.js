5ae050362eb4e5aee405a83b0327c96573db3346
Ronald_Queirox <antonio.queiroz47@aluno.ce.gov.br>
2025-04-14 21:20:33 -0300

    1.0.0.3

--- Aulas/Aula-09b/json.js
+++ Aulas/Aula-09b/json.js
@@ -8,3 +8,7 @@ function Pessoa(nome, idade, matricula, id_cad) {
   let pessoa1 = new Pessoa('Maria', 15, 2442, 12);
   console.log(pessoa1);
   console.log(JSON.stringify(pessoa1))
+
+  let pessoa2 = new Pessoa('José', 14, 32, 94)
+  console.log(pessoa2)
+  console.log(JSON.stringify(pessoa2))
