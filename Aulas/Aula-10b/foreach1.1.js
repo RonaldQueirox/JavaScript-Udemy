1c3c8dda6147ca26290bdcd424ef54d55cc7cd5e
Ronald_Queirox <antonio.queiroz47@aluno.ce.gov.br>
2025-04-16 22:46:52 -0300

    1.0.0.5

--- Aulas/Aula-10b/foreach1.1.js
+++ Aulas/Aula-10b/foreach1.1.js
@@ -2,3 +2,8 @@ const APROVADOS = ['Laura', 'Ana', 'Raquel', 'Daniel']
 APROVADOS.forEach(function(nome, indice) {
     console.log(`${indice + 1} - ${nome}`)
 })
+
+APROVADOS.forEach(nome => console.log(nome))
+
+const Exibir_Aprovados = APROVADO => console.log(APROVADO)
+APROVADOS.forEach(Exibir_Aprovados)
---
+++ Aulas/Aula-10b/foreach1.2.js
@@ -0,0 +1,7 @@
+const APROVADOS = ['Laura', 'Ana', 'Raquel', 'Daniel']
+const forEach2 = function(APROVADOS) {
+    for(const ITEM of APROVADOS) {
+        console.log(ITEM)
+    }
+}
+forEach2(APROVADOS)
